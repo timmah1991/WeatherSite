7fd396f7796172a381817cb43bdd9e31de3b7bc5
Thomas Kittredge <thomaskittredge@T13E.domain>
2019-02-27 02:10:54 -0700

    added project dependencies and basic code

---
+++ index.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+
+let apiKey = argv.k || '0000';
+let city = argv.c || 'denver';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
