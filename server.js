31ba1b09286ad43c18ba6b60131fca14a4c5fc37
Thomas Kittredge <thomaskittredge@Thomass-MacBook-Pro.local>
2019-02-28 01:24:10 -0700

    Added API key field to webpage

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let apiKey = req.body.apikey;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
